f4bc4d97fb1566386b298fd2fb0afbccb18bc52c
tvru <167865212+tvru@users.noreply.github.com>
2025-04-14 12:04:29 +0300

    Update store.js

--- store/store.js
+++ store/store.js
@@ -14,7 +14,7 @@
     });
     function addStore() {
         if (Lampa.Settings.main && !Lampa.Settings.main().render().find('[data-component="pirate_store"]').length) {
-            var field = "<div class=\"settings-folder selector\" data-component=\"pirate_store\" data-static=\"true\">\n\t\t\t<div class=\"settings-folder__icon\">\n\t\t\t\t<svg width=\"490.00000000000006\" height=\"490.00000000000006\" xmlns=\"http://www.w3.org/2000/svg\" xml:space=\"preserve\" version=\"1.1\"><path id=\"svg_1\" fill=\"#ff0000\" d=\"m31.25029,229.68749l427.49942,0l0,30.625l-427.49942,0l0,-30.625z\"/><path id=\"svg_5\" d=\"m-62.9171,180.83232\" opacity=\"NaN\" stroke=\"#ff0000\" fill=\"white\"/></svg>\n\t\t\t</div>\n\t\t\t<div class=\"settings-folder__name\">"+Lampa.Lang.translate('pirate_store')+"</div>\n\t\t</div>";
+            var field = "<div class=\"settings-folder selector\" data-component=\"pirate_store\" data-static=\"true\">\n\t\t\t<div class=\"settings-folder__icon\">\n\t\t\t\t<svg width=\"512\" height=\"512\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><g id=\"Complete\"><g id=\"minus\"><line fill=\"none\" stroke=\"#000000\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" x1=\"4\" x2=\"20\" y1=\"12\" y2=\"12\"/></g></g></svg>\n\t\t\t</div>\n\t\t\t<div class=\"settings-folder__name\">"+Lampa.Lang.translate('pirate_store')+"</div>\n\t\t</div>";
             Lampa.Settings.main().render().find('[data-component="more"]').after(field);
             Lampa.Settings.main().update();
         }
